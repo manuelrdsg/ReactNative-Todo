819c268d17c839d057d6b9d63726cea21534e3c9
manuelrdsg <manuelrdsg@gmail.com>
2018-02-18 20:30:35 +0000

    Moved to AsyncStorage

--- TodoApp/App.js
+++ TodoApp/App.js
@@ -2,12 +2,11 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   Text,
-  //TextInput,
   View,
   StyleSheet,
-  //Button,
   FlatList,
-  KeyboardAvoidingView
+  KeyboardAvoidingView,
+  AsyncStorage
 } from 'react-native';
 import { Button, FormInput, Header } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -17,50 +16,76 @@ export default class ToDoApp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: '',
-      tasks: []
+      tasks: [],
+      text: ''
     };
   }
 
-  addToDo = () => {
+  async _updateTodos() {
+    let response = await AsyncStorage.getItem('Tasks');
+    let tasks = await JSON.parse(response) || [];
 
+    this.setState({
+      tasks
+    });
+
+  };
+
+  async _saveTodos(tasks) {
+    await AsyncStorage.setItem('Tasks', JSON.stringify(tasks));
+  }
+
+  async _debugTodos() {
+    console.log('********DEBUG - AsyncStorage*******')
+    let response = await AsyncStorage.getItem('Tasks');
+    let tasks = await JSON.parse(response) || [];
+    console.log(tasks);
+    console.log('********************');
+  }
+
+  _debugTodosState() {
+    console.log('%%%%%%%DEBUG - State%%%%%%%')
     console.log(this.state.tasks);
+    console.log('%%%%%%%%%%%%%%%%%%%%%%%%%%%');
+  }
+
+  addToDo() {
+
     let notEmpty = this.state.text.trim().length > 0;
 
     if (notEmpty) {
-      this.setState(
-        prevState => {
-          let { text, tasks } = prevState;
-          return {
-            text: "",
-            tasks: tasks.concat({ key: Math.random(), text: text })
-          };
-        }
-      );
+
+      const tasks = this.state.tasks.concat({ key: Math.random(), text: this.state.text });
+      this._saveTodos(tasks);
+      this._updateTodos();
     }
+
+    this.changeTextInput("");
+  };
+
+  deleteToDo(index) {
+    var array = this.state.tasks;
+    array.splice(index, 1);
+    this.setState({ tasks: array })
+
+    this._saveTodos(this.state.tasks);
+    this._updateTodos();
+
+  };
+
+  changeTextInput(text) {
+    this.setState({
+      text
+    });
     this.input.clearText();
-  };
-
-  deleteToDo = key => {
-    this.setState(
-      prevState => {
-
-        let array = prevState.tasks.slice();
-        array.splice(key, 1);
-
-        return {
-          tasks: array
-        };
-      }
-    )
-  };
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <Header centerComponent={{ text: 'TodoApp', style: { color: '#fff', fontSize: 18 } }} />
-        {/* <Text style={styles.head}>Todo App</Text>
-        <View style={styles.line} /> */}
+        {/* <Icon name="paper-plane" size={30} color="black" onPress={() => this._debugTodos()} />
+        <Icon name="paper-plane" size={30} color="red" onPress={() => this._debugTodosState()} /> */}
         <FlatList
           data={this.state.tasks}
           renderItem={({ item, index }) =>
@@ -77,15 +102,6 @@ export default class ToDoApp extends Component {
           behavior='padding'
           style={styles.row}
         >
-          {/* <TextInput
-            style={styles.text_imput}
-            ref={input => { this.textInput = input }}
-            placeholder="Enter new task"
-            onChangeText={(text) => this.setState({ text })}
-            onSubmitEditing={
-              () => this.addToDo()
-            }
-          /> */}
           <FormInput
             ref={input => this.input = input}
             containerStyle={styles.text_imput}
@@ -101,6 +117,7 @@ export default class ToDoApp extends Component {
       </View>
     );
   }
+
 }
 
 // skip this line if using Create React Native App
